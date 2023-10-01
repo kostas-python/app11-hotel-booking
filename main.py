5724d4141cdd72ca15560ec6243298e6e542a89b
kostas-python <diamantopoulos.kostas@hotmail.com>
2023-10-01 13:50:35 +0100

    Day 42: Added spa package

--- main.py
+++ main.py
@@ -25,6 +25,11 @@ class Hotel:
             return False
 
 
+class SpaHotel(Hotel):
+    def book_spa_package(self):
+        pass
+
+
 class ReservationTicket:
     def __init__(self, customer_name, hotel_object):
         self.customer_name = customer_name
@@ -62,9 +67,25 @@ class SecureCreditCard(CreditCard):
             return False
 
 
+class SpaTicket():
+    def __init__(self, customer_name, hotel_object):
+        self.customer_name = customer_name
+        self.hotel = hotel_object
+
+    def generate(self):
+        content = f"""
+        Thank you for your spa reservation!
+        Here are your booking data:
+        Name: {self.customer_name}
+        Hotel: {self.hotel.name}
+        """
+        return content
+
+
 print(df)
 hotel_ID = input("Enter the id of the hotel: ")
-hotel = Hotel(hotel_ID)
+hotel = SpaHotel(hotel_ID)
+
 if hotel.available():
     credit_card = SecureCreditCard(number="1234567890123456")
     if credit_card.validate(expiration="12/26", holder="JOHN SMITH", cvc="123"):
@@ -73,9 +94,18 @@ if hotel.available():
             name = input("Enter your name: ")
             reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
             print(reservation_ticket.generate())
+            spa = input("Do you want to book a spa package? ")
+            if spa == "yes":
+                hotel.book_spa_package()
+                spa_ticket = SpaTicket(customer_name=name, hotel_object=hotel)
+                print(spa_ticket.generate())
         else:
             print("Credit card authentication failed")
     else:
         print("There is a problem with your credit card")
 else:
     print("Hotel is not available.")
+
+
+
+
